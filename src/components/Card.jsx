d861a509edb57c159a64f115777230ff2c0afd70
Poorvanshi-sahu <poorvanshisahu@gmail.com>
2023-12-10 17:37:05 +0530

    adding all the changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import { FaRegFileAlt } from "react-icons/fa";
+import { GoDownload } from "react-icons/go";
+import { IoCloseOutline } from "react-icons/io5";
+import { motion } from "framer-motion";
 
-const Card = () => {
+const Card = ({ data, reference }) => {
   return (
     <>
-      <div className="h-64 w-56 rounded-[30px] bg-slate-200">bhj</div>
+      <motion.div
+        drag
+        dragConstraints={reference}
+        whileHover={{
+          scale: 1.1,
+          transition: { duration: 1 },
+        }}
+        className="h-64 w-52 rounded-[45px] bg-zinc-900 p-8 relative overflow-hidden text-slate-200 cursor-pointer"
+      >
+        <FaRegFileAlt />
+        <h1 className="mt-[12px] leading-tight font-semibold text-sm text-truncate">
+          {data.text.length > 100
+            ? data.text.split("").slice(0, 100).join("") + "..."
+            : data.text}
+        </h1>
+        <div className="footer absolute bottom-0 left-0 w-full">
+          <div className="upper mb-4 px-8 flex text-center justify-between">
+            <h3>{data.fileSize}</h3>
+            <span className="h-6 w-6 rounded-full bg-zinc-600 flex items-center justify-center cursor-pointer">
+              {data.download && <GoDownload color="white" />}
+              {data.close && <IoCloseOutline color="white" />}
+            </span>
+          </div>
+          {data.tag.showTag && (
+            <div
+              className={`lower py-2 ${
+                data.tag.tagColor === "blue" ? "bg-blue-900" : "bg-green-700"
+              }  flex justify-center items-center text-slate-50`}
+            >
+              {data.tag.tagText}
+            </div>
+          )}
+        </div>
+      </motion.div>
     </>
   );
 };
--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -1,11 +1,98 @@
-import React from "react";
+import { useRef } from "react";
 import Card from "./Card";
 
 const Forground = () => {
+  const data = [
+    {
+      text: "Lorem ipsum dolor, sit amet consec.",
+      fileSize: "4mb",
+      close: true,
+      download: false,
+      tag: { showTag: true, tagText: "Download Now", tagColor: "blue" },
+    },
+    {
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elitCupiditate enim quam eveniet aspernatur velit perferendis Lorem ipsum dolor, sit amet consectetur adipisicing elitCupiditate enim quam eveniet aspernatur velit perferendis",
+      fileSize: "6.0mb",
+      close: true,
+      download: false,
+      tag: { showTag: true, tagText: "Download Now", tagColor: "green" },
+    },
+    {
+      text: "Lorem ipsum dolor, sit amet e enim quam eveniet aspernatur velit perferendis",
+      fileSize: "2mb",
+      close: false,
+      download: true,
+      tag: { showTag: true, tagText: "Download Now", tagColor: "blue" },
+    },
+    {
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elitCupiditate enim quam eveniet aspernatur velit perferendis Lorem ipsum dolor, sit amet consectetur adipisicing elitCupiditate enim quam eveniet aspernatur velit perferendis",
+      fileSize: "6.0mb",
+      close: true,
+      download: false,
+      tag: { showTag: true, tagText: "Download Now", tagColor: "green" },
+    },
+    {
+      text: "Lorem ipsum dolor, sit amet e enim quam eveniet aspernatur velit perferendis",
+      fileSize: "2mb",
+      close: false,
+      download: true,
+      tag: { showTag: true, tagText: "Download Now", tagColor: "blue" },
+    },
+    {
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elitCupiditate enim quam eveniet aspernatur velit perferendis",
+      fileSize: "4mb",
+      close: true,
+      download: false,
+      tag: { showTag: true, tagText: "Download Now", tagColor: "blue" },
+    },
+    {
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elitCupiditate enim quam eveniet aspernatur velit perferendis Lorem ipsum dolor, sit amet consectetur adipisicing elitCupiditate enim quam eveniet aspernatur velit perferendis",
+      fileSize: "6.0mb",
+      close: true,
+      download: false,
+      tag: { showTag: true, tagText: "Download Now", tagColor: "green" },
+    },
+    {
+      text: "Lorem ipsum dolor, sit amet e enim quam eveniet aspernatur velit perferendis",
+      fileSize: "2mb",
+      close: false,
+      download: true,
+      tag: { showTag: true, tagText: "Download Now", tagColor: "blue" },
+    },
+    {
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elitCupiditate enim quam eveniet aspernatur velit perferendis",
+      fileSize: "4mb",
+      close: true,
+      download: false,
+      tag: { showTag: true, tagText: "Download Now", tagColor: "blue" },
+    },
+    {
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elitCupiditate enim quam eveniet aspernatur velit perferendis Lorem ipsum dolor, sit amet consectetur adipisicing elitCupiditate enim quam eveniet aspernatur velit perferendis",
+      fileSize: "6.0mb",
+      close: true,
+      download: false,
+      tag: { showTag: true, tagText: "Download Now", tagColor: "green" },
+    },
+    {
+      text: "Lorem ipsum dolor, sit amet e enim quam eveniet aspernatur velit perferendis",
+      fileSize: "2mb",
+      close: false,
+      download: true,
+      tag: { showTag: true, tagText: "Download Now", tagColor: "blue" },
+    },
+  ];
+
+  const constraintsRef = useRef(null);
+
   return (
     <>
-      <div className="absolute w-full h-full bg-sky-100/2 z-[3] top-0 left-0">
-        <Card />
+      <div
+        ref={constraintsRef}
+        className="absolute w-full h-full overflow-y-auto overflow-x-hidden bg-sky-100/2 z-[3] top-0 left-0 flex gap-7 flex-wrap p-5"
+      >
+        {data.map((item, index) => {
+          return <Card data={item} key={index} reference={constraintsRef} />;
+        })}
       </div>
     </>
   );
